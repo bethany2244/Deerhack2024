81e7aa9170a31d067b763d86f8f43b573ffa3275
hoyohai <62492287+hoyohai@users.noreply.github.com>
2024-02-17 21:39:12 -0500

    made markers undraggable

--- marker.js
+++ marker.js
@@ -1,3 +1,4 @@
+import anime from "./node_modules/animejs/lib/anime.es.js";
 import { currentMarker } from "./script.js";
 
 export function constructMarker(c, long, lat, map, name, currentMarker) {
@@ -6,7 +7,7 @@ export function constructMarker(c, long, lat, map, name, currentMarker) {
   console.log(name);
   const marker = new mapboxgl.Marker({
     color: c,
-    draggable: true,
+    draggable: false
   })
 
     .setLngLat([long, lat])
